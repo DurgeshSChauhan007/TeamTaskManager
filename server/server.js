0ec3719e5c32d4e1e1da4aef26bd2f908777a78a
DurgeshSChauhan007 <durgeshsinghchauhan97.4@gmail.com>
2026-05-01 15:34:29 +0530

    commit server.js

--- server/server.js
+++ server/server.js
@@ -10,13 +10,31 @@ connectDB();
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/projects", require("./routes/projectRoutes"));
-app.use("/api/tasks", require("./routes/taskRoutes"));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running`);
+app.use(
+  "/api/auth",
+  require("./routes/authRoutes")
+);
+
+app.use(
+  "/api/projects",
+  require("./routes/projectRoutes")
+);
+
+app.use(
+  "/api/tasks",
+  require("./routes/taskRoutes")
+);
+
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running on port ${PORT}`
+  );
 });
